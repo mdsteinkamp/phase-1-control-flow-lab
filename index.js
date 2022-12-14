b3bd48ce03157eee80c79457795d9e5a103f288a
Matthew Steinkamp <mdsteinkamp@gmail.com>
2022-12-13 19:01:57 -0800

    commit tree 8c67cd3b3954db9ff2f4d49b1b5fcee3766b356c

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  let message;
+  if (rideLength > 2500) {
+    message = 'No can do.';
+  } else if (rideLength > 2000) {
+      message = 'I will gladly take your thirty bucks.';
+  } else if (rideLength > 400 ) {
+    message = 'That will be twenty bucks.';
+  } else if (rideLength <= 400) {
+    message = 'This one is on me!';
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  return cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  let response;
+  switch (tipAmount) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+  }
+  return response;
 }
